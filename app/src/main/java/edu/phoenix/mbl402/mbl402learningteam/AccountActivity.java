d46dc23a3f964c9936dbd20238dc0b180593be83
RobLuc42 <rjl2483@gmail.com>
2018-11-11 13:22:54 -0500

    Mainactivity -Add Homescreen image -Add Account button -Add Map button -Remove Title bar
    Added AccountActivity

---
+++ app/src/main/java/edu/phoenix/mbl402/mbl402learningteam/AccountActivity.java
@@ -0,0 +1,13 @@
+package edu.phoenix.mbl402.mbl402learningteam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account);
+    }
+}
